9f13256f4721a627541774135f32d32591763b7a
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-06-16 19:23:40 +0200

    feat: SEO Metadata component (#20)

---
+++ src/lib/components/app/misc/SeoMetadata.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import {
+		SITE_URL,
+		SITE_TITLE,
+		SITE_DESCRIPTION,
+		OG_IMAGE_PATH,
+		OG_IMAGE_ALT
+	} from '$lib/constants/site';
+</script>
+
+<svelte:head>
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={SITE_TITLE} />
+	<meta property="og:description" content={SITE_DESCRIPTION} />
+	<meta property="og:url" content={SITE_URL} />
+	<meta property="og:image" content={OG_IMAGE_PATH} />
+	<meta property="og:image:alt" content={OG_IMAGE_ALT} />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={SITE_TITLE} />
+	<meta name="twitter:description" content={SITE_DESCRIPTION} />
+	<meta name="twitter:image" content={OG_IMAGE_PATH} />
+</svelte:head>
