f940a9aef646cf48902d8ed22a86db3665071e63
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-06-22 16:34:44 +0200

    feat: Add backdrop blur to site header (#27)

--- src/lib/components/app/navigation/SiteHeader.svelte
+++ src/lib/components/app/navigation/SiteHeader.svelte
@@ -15,7 +15,7 @@
 	}
 </script>
 
-<header class="flex items-center justify-between p-6 mx-auto w-full max-w-5xl sticky top-0 md:px-12">
+<header class="flex items-center justify-between p-6 mx-auto w-full max-w-5xl sticky top-0 md:px-12 backdrop-blur-md z-9999">
 	<a href={resolve('/')}>
 		<Logo --logo-height="1.5rem" />
 	</a>
